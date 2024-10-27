15105f2035b8a15ebbd41892edd651b4071df185
whisky Dark <illuminati.w@gmail.com>
2024-10-28 07:13:26 +0800

    updated and complete - wait for test

--- API/main2.py
+++ API/main2.py
@@ -1,5 +1,5 @@
-# get_day_off.py Ver 0.2
-# 2024.10.25
+# get_day_off.py Ver 0.3
+# 2024.10.28
 # Get Typhone Day_off announcement from DGPA at https://www.dgpa.gov.tw/typh/daily/nds.html
 # 1. get update_time
 # 2. check if there is announcement
@@ -17,6 +17,7 @@
 #           }
 #   }
 
+import json
 import os
 from datetime import datetime, timedelta
 
@@ -36,6 +37,7 @@ root_dir = os.path.dirname(curr_path)
 
 def get_day_off_table(html=""):
     results = {
+        "url": url,
         "checked_at": datetime.now().strftime("%Y.%m.%d %H:%M:%S"),
         "updated_at": "",
         "status_code": 0,
@@ -69,11 +71,13 @@ def get_day_off_table(html=""):
     )
 
     today = datetime.strptime(results["updated_at"], "%Y.%m.%d %H:%M:%S").date()
+    today_str = today.strftime("%Y.%m.%e")
     tomorrow = today + timedelta(days=1)
+    tomorrow_str = tomorrow.strftime("%Y.%m.%e")
 
     city_table = s.find("tbody", class_="Table_Body").find_all("tr")
 
-    print([x for x in city_table if len(x.find_all("td")) > 1])
+    # print([x for x in city_table if len(x.find_all("td")) > 1])
     # 無停班停課
     try:
         ann_status = city_table[0].find("h2").text
@@ -90,6 +94,7 @@ def get_day_off_table(html=""):
     for city in city_table:
         city_cols = city.find_all("td")
         city_final = {}
+        city_final_list = []
 
         # 非停班停課公告
         if len(city_cols) != 2:
@@ -116,16 +121,29 @@ def get_day_off_table(html=""):
             for status_row in city_status:
                 if ":" not in status_row:
                     # 地方政府單一公告
-                    city_final[city_name] = status_row
+                    city_final[city_name] = reformat_status(
+                        status_row, today_str, tomorrow_str
+                    )
+                    city_final_list.append([city_name, *city_final[city_name]])
                 else:
                     # 多重公告，地區或學校停班聽課
                     sub_zone, sub_status = [x.strip() for x in status_row.split(":")]
+                    if city_name in sub_zone or city_name + "立" in sub_zone:
+                        sub_zone = sub_zone.replace(city_name + "立", "")
+                        sub_zone = sub_zone.replace(city_name, "")
+
                     # Multiple zones in one row
                     if "、" in sub_zone:
                         for zone_name in sub_zone.split("、"):
-                            city_final[zone_name] = sub_status
+                            city_final[zone_name] = reformat_status(
+                                sub_status, today_str, tomorrow_str
+                            )
+                            city_final_list.append([zone_name, *city_final[zone_name]])
                     else:
-                        city_final[sub_zone] = sub_status
+                        city_final[sub_zone] = reformat_status(
+                            sub_status, today_str, tomorrow_str
+                        )
+                        city_final_list.append([sub_zone, *city_final[sub_zone]])
 
                 # finale format: 'city_name/zone_name' = city(zone)_status
                 # 今天停止上班、停止上課
@@ -140,14 +158,50 @@ def get_day_off_table(html=""):
             print(f":: {city_name} - {city_status}")
             continue
 
-        results["data"][city_name] = city_final
+        results["data"][city_name] = city_final_list
     return results
+
+
+def reformat_status(status, today, tomorrow):
+    formated_status = []
+    if "今天" in status:
+        formated_status.append(today)
+        status = status.replace("今天", "")
+    elif "明天" in status:
+        formated_status.append(tomorrow)
+        status = status.replace("明天", "")
+    else:
+        formated_status.append("No Date")
+
+    if "、" in status:
+        work_day, school_day = status.split("、")
+    else:
+        print(status)
+        work_day, school_day = "照常"
+
+    if "停止" in work_day:
+        formated_status.append("0")
+    else:
+        formated_status.append("1")
+    if "停止" in school_day:
+        formated_status.append("0")
+    else:
+        formated_status.append("1")
+
+    return formated_status
+
+
+def save_to_dataset(results):
+    date_now = datetime.now().strftime("%Y-%m-%d")
+    with open(
+        os.path.join(root_dir, "Data", date_now + ".json"), "w", encoding="utf-8"
+    ) as f:
+        json.dump(results, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
     with open("..\\Data\\dummy.html", "r") as f:
         dummy = f.read()
 
-    results = get_day_off_table(dummy)
-    print(results)
-    # save_to_dataset(results)
+    results = get_day_off_table()
+    save_to_dataset(results)
